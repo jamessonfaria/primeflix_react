cdb40fd6d8e6abea033fe3431933129dece1b310
Jamesson Faria <jamessonjr@gmail.com>
2023-06-11 15:42:28 -0300

    Developed adjusts in home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../../services/api";
+import { Link } from 'react-router-dom';
+import './home.css';
 
 function Home() {
 
@@ -10,13 +12,13 @@ function Home() {
         async function loadFilmes(){
             const response = await api.get("movie/now_playing", {
                 params: {
-                    api_key: "<YOUR KEY>>",
+                    api_key: "<YOUR KEY>",
                     language: "pt-BR",
                     page: 1
                 }
             });
 
-            console.log(response.data.results);
+            setFilmes(response.data.results.slice(0,10));
         }
 
         loadFilmes();
@@ -24,8 +26,20 @@ function Home() {
     }, []);
 
     return(
-        <div>
-            <h1>Bem vindo a HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} 
+                                alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    );
+                })}
+            </div>
+            
         </div>
     );
 }
