f88edaa953e36cf3064e7bc89635d3302c448138
Jamesson Faria <jamessonjr@gmail.com>
2023-06-11 15:14:02 -0300

    Added axios in project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+import api from "../../services/api";
 
 function Home() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "<YOUR KEY>>",
+                    language: "pt-BR",
+                    page: 1
+                }
+            });
+
+            console.log(response.data.results);
+        }
+
+        loadFilmes();
+
+    }, []);
+
     return(
         <div>
             <h1>Bem vindo a HOME</h1>
